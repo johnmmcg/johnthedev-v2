07c6539123d03090a20bcf3ab57eb9c485c402eb
John <john.maurer.mcguinness@gmail.com>
2022-10-11 15:17:11 -0400

    font and basic dark theme for UnderConstruction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,js,astro}"
   ],
   theme: {
+    fontFamily: {
+      'sans': ["'Nunito'", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
