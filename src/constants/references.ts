4c417625c034e845ff6385aa88079581e2f49e97
John <john.maurer.mcguinness@gmail.com>
2023-05-08 16:26:49 -0400

    static ref images added

--- src/constants/references.ts
+++ src/constants/references.ts
@@ -1,8 +1,7 @@
 export default [
   {
     name: 'Emma Bostian',
-    image_url:
-      'https://media.licdn.com/dms/image/C4D03AQEvPrBvNujANA/profile-displayphoto-shrink_400_400/0/1603366792796?e=1686182400&v=beta&t=eSM23N8yNJ9s9LW6RU8BVwtYua7rq_dzcb88ZMtaLYw',
+    image_url: '/static/refs/avatar_emma.jpeg',
     role: 'Engineering Manager',
     company: 'Spotify',
     text: "I don't have enough kind words to describe John. He was always willing to jump into difficult tasks. And he made everyone feel welcomed and included. Any company would be lucky to have John. I hope we can work together in the future.",
@@ -10,8 +9,7 @@ export default [
   },
   {
     name: 'Albetro Núñez Acosta',
-    image_url:
-      'https://media.licdn.com/dms/image/C4D03AQFBiVygBS5L4w/profile-displayphoto-shrink_100_100/0/1516823323464?e=1681948800&v=beta&t=CoQYjCBdKN2hGiFxJA9wEttZn8PUxsrLrxNXNpC-R_0',
+    image_url: '/static/refs/avatar_alberto.jpeg',
     role: 'Senior Engineering Manager',
     company: 'Spotify',
     text: "John is a committed professional and a fantastic colleague. John was hired into a newly created team and soon stood out as someone who takes ownership, breaks down problems and gets work done. He's a pleasure to communicate with, and creates a great team atmosphere.",
@@ -19,8 +17,7 @@ export default [
   },
   {
     name: 'Robert Sliwa',
-    image_url:
-      'https://media.licdn.com/dms/image/C5103AQH5XHsE_xT0Pg/profile-displayphoto-shrink_400_400/0/1516349636068?e=1686182400&v=beta&t=tmkRsMhOR_8vtHQdkOQN5KhOAo5bxobccwWjcIseDcw',
+    image_url: '/static/refs/avatar_rob.jpeg',
     role: 'Senior Systems Engineer',
     company: 'Somos & Comcast',
     text: 'John is one of the most creative front-end developers I ever worked with. When faced with the need to learn new technologies, he never hesitated and was very quick in getting himself up to speed. He is able to motivate teams, and everyone wants to work with him.',
@@ -28,8 +25,7 @@ export default [
   },
   {
     name: 'Danielle Nourok',
-    image_url:
-      'https://media.licdn.com/dms/image/C4E03AQEMSZRFhyJdIQ/profile-displayphoto-shrink_400_400/0/1517533808518?e=1686182400&v=beta&t=xXrGMUM1SO2iof-VLCVwIxnOKye9U8QL1tt5p6XL5F0',
+    image_url: '/static/refs/avatar_danielle.jpeg',
     role: 'Engineering Manager',
     company: 'Spotify & Disney',
     text: 'John is an example of a team player. He is an excellent communicator, supportive to his teammates and a very positive person. He is a problem solver and excels at breaking down technical requirements, working cross team and debugging tough problems.',
@@ -37,8 +33,7 @@ export default [
   },
   {
     name: 'Steven Bruno',
-    image_url:
-      'https://media.licdn.com/dms/image/C4E03AQFImHjy3QRtHA/profile-displayphoto-shrink_400_400/0/1638245525010?e=1686182400&v=beta&t=ZZWOj4gNJPBvejL_tSgqSZE5bNl4JG7ZEHL61_lj2Qc',
+    image_url: '/static/refs/avatar_steven.jpeg',
     role: 'Product Designer',
     company: 'Spotify',
     text: 'John was an exceptional teammate and engineering partner. He is knowledgeable, efficient, and cares deeply about quality engineering. I really enjoyed working alongside John - any team would be lucky to have him as a teammate.',
@@ -46,8 +41,7 @@ export default [
   },
   {
     name: 'Steven McKeon',
-    image_url:
-      'https://media.licdn.com/dms/image/C4E03AQFoIRt_1knqwQ/profile-displayphoto-shrink_400_400/0/1604251048311?e=1686182400&v=beta&t=moMFGAhKYhymDhxOnUehnQSbaazhB8OM8WkFaA3hUrk',
+    image_url: '/static/refs/avatar_mac.jpeg',
     role: 'CEO',
     company: 'MacGuyverTech',
     text: 'John was routinely the most creative and capable web developer we had. His coding was excellent and his designs were unique and innovative, so clients were always happy. He was always thoughtful and considerate, an excellent team player.',
@@ -55,8 +49,7 @@ export default [
   },
   {
     name: 'Brian Isely',
-    image_url:
-      'https://media.licdn.com/dms/image/C4E03AQFIZOJbydZZug/profile-displayphoto-shrink_400_400/0/1614623125118?e=1686182400&v=beta&t=Mjz0hCeUGJPdj3rImy7RXteSJ89Y2sizkJazEI27YNU',
+    image_url: '/static/refs/avatar_brian.jpeg',
     role: 'Executive VP',
     company: 'Center City Film & Video',
     text: 'John is always on time, focused, courteous, extremely intelligent, humble and hard working. I give John my highest recommendation and am certain he will be an asset to any organization that is lucky enough to employ him.',
