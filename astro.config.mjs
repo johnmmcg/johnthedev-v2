cde300d4a4a7ccc09d44ee65a78332ce02b5f9a1
John <john.maurer.mcguinness@gmail.com>
2023-09-03 19:41:43 -0400

    updates for netlify build and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: netlify(),
     site: 'https://www.johnthedev.com',
     integrations: [
         react(),
-        tailwind()
+        tailwind(),
     ]
 });
